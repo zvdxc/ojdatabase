42791f2ef88f7187794a2e4260501761019be648
zvdxc <zvdxc11@gmail.com>
2022-06-16 20:39:25 +0200

    Delete opentoinstall.bat

--- OpenJoffice Database/installer/opentoinstall.bat
+++
@@ -1,7 +0,0 @@
-md OpenJoffice Database
-cd OpenJoffice Database
-curl https://raw.githubusercontent.com/zvdxc/OpenJoffice-database/main/OpenJoffice%20Database/DATABASE.bat >> RUNME.bat
-cls
-@echo off
-echo fertig!
-pause
