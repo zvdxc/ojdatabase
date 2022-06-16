c68c006637f16f010ef15d85ee084fea60dde431
zvdxc <zvdxc11@gmail.com>
2022-06-16 20:37:25 +0200

    Create opentoinstall.bat

---
+++ OpenJoffice Database/installer/opentoinstall.bat
@@ -0,0 +1,7 @@
+md OpenJoffice Database
+cd OpenJoffice Database
+curl https://raw.githubusercontent.com/zvdxc/OpenJoffice-database/main/OpenJoffice%20Database/DATABASE.bat >> RUNME.bat
+cls
+@echo off
+echo fertig!
+pause
